Fixed silent exit when no packets are received

lucid_cnn.py
@@ -373,26 +373,23 @@ def main(argv):
 
         while (True):
             samples = process_live_traffic(cap, args.dataset_type, labels, max_flow_len, traffic_type="all", time_window=time_window)
-            if len(samples) == 0:
-                break
+            if len(samples) > 0:
+                X,Y_true,keys = dataset_to_list_of_fragments(samples)
+                X = np.array(normalize_and_padding(X, mins, maxs, max_flow_len))
+                if labels is not None:
+                    Y_true = np.array(Y_true)
+                else:
+                    Y_true = None
 
-            X,Y_true,keys = dataset_to_list_of_fragments(samples)
-            X = np.array(normalize_and_padding(X, mins, maxs, max_flow_len))
-            if labels is not None:
-                Y_true = np.array(Y_true)
-            else:
-                Y_true = None
+                X = np.expand_dims(X, axis=3)
+                pt0 = time.time()
+                Y_pred = np.squeeze(model.predict(X, batch_size=2048) > 0.5)
+                pt1 = time.time()
+                prediction_time = pt1 - pt0
 
-            X = np.expand_dims(X, axis=3)
-            pt0 = time.time()
-            Y_pred = np.squeeze(model.predict(X, batch_size=2048) > 0.5)
-            pt1 = time.time()
-            prediction_time = pt1 - pt0
-
-            [packets] = count_packets_in_dataset([X])
-            report_results(Y_true, Y_pred, packets, model_name_string,
-                           data_source, stats_file, prediction_time)
-            continue
+                [packets] = count_packets_in_dataset([X])
+                report_results(Y_true, Y_pred, packets, model_name_string,
+                               data_source, stats_file, prediction_time)
 
 def report_results(Y_true, Y_pred,packets, model_name, dataset_filename, stats_file,prediction_time):
     ddos_rate = '{:04.3f}'.format(sum(Y_pred)/Y_pred.shape[0])